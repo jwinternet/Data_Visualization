efbed5efce460125c3a145ace42e91fa27f74c5d
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-03-10 15:12:56 -0400

    Added colors.

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,5 +1,5 @@
 """
-Data Visualization Project - mpl_squares
+Data Visualization Project - scatter_squares
 """
 ###############################################################################
 
@@ -7,7 +7,7 @@ __author__ = "Jared Winter"
 __copyright__ = "Copyright 2020, jwinternet"
 __credits__ = ""
 __license__ = "GNU General Public License v3.0"
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 __updated__ = "3/10/2020"
 __email__ = "jaredwinter2015@outlook.com"
 __status__ = "DEV"
@@ -18,19 +18,22 @@ import matplotlib.pyplot as plt
 
 ###############################################################################
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x ** 2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
 
 # Set chart title and label axes.
-ax.set_title("Square Numbers", fontsize=28)
-ax.set_xlabel("Value", fontsize=18)
-ax.set_ylabel("Square of Value", fontsize=18)
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
